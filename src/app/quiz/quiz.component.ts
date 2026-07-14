571a60dfb238d47ed807bc3b515f202e11925f51
Nguyễn Đức Anh <78894062+kiencang@users.noreply.github.com>
2026-07-14 14:51:20 +0700

    Delete src/app/quiz directory

--- src/app/quiz/quiz.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  selector: 'app-quiz',
-  templateUrl: './quiz.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class QuizComponent {
-}
